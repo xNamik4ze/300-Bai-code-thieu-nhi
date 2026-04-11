4e3a3da95b5bd84720482e60f730cbef72f01d6f
xNamik4ze <iceminato31@gmail.com>
2026-04-11 07:17:28 +0700

    Add Two Numbers

--- Python/S0002.py
+++ Python/S0002.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy =  ListNode(0)
+        dummy = ListNode(0)
         cur = dummy
         carry = 0
         while l1 or l2 or carry:
