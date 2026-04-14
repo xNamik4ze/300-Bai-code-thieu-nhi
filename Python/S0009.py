bc07d1199065eec79d20eaa5d333a58ce3b92892
xNamik4ze <iceminato31@gmail.com>
2026-04-14 14:03:52 +0700

    Palindrome Number

--- Python/S0009.py
+++ Python/S0009.py
@@ -1,10 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0: return False
-        original = x
+        if x < 0 or (x % 10 == 0 and x != 0): return False
         reversed = 0
-        while x > 0:
-            temp = x % 10
-            reversed = reversed * 10 + temp
+        while x > reversed:
+            reversed = reversed * 10 + x % 10
             x //= 10
-        return original == reversed
+        return x == reversed or x == reversed // 10
