fc8bc5d3fe499aed08ee3b0072e82dc29eba8c24
xNamik4ze <iceminato31@gmail.com>
2026-05-05 21:01:10 +0700

    Search Insert Position

--- Python/S0035.py
+++ Python/S0035.py
@@ -1,6 +1,6 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left = 0
+        left = 0 
         right = len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
