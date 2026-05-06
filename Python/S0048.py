da9710843d99b5c221ac235093859860d2e05629
xNamik4ze <iceminato31@gmail.com>
2026-05-06 21:43:43 +0700

    Rotate Image

--- Python/S0048.py
+++ Python/S0048.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(n):
             for j in range(i + 1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        for i in range(n):
+        for i in range(n): 
             j = 0
             while j < n // 2:
                 matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
