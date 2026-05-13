36275ad5ca3a03b8aa0339f325bed9a945129a6d
xNamik4ze <iceminato31@gmail.com>
2026-05-13 20:17:08 +0700

    Valid Palindrome

---
+++ C++/S0125.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) l++;
+            while (l < r && !isalnum(s[r])) r--; 
+            if (tolower(s[l]) != tolower(s[r])) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
