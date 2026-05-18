7657fcec65f580f289ae27b52749f3ce2a442602
Nguyen Duc Hieu <iceminato31@gmail.com>
2026-05-18 21:17:49 +0700

    Rotate Array

--- Python/S0189.py
+++ Python/S0189.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k %= len(nums)
+        k %= len(nums) 
         nums.reverse()
         nums[:k] = reversed(nums[:k])
         nums[k:] = reversed(nums[k:])
