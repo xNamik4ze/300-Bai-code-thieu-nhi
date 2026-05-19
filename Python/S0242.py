1ad2e9c447d33215063c345bae4d750d1ed43ce2
Nguyen Duc Hieu <iceminato31@gmail.com>
2026-05-19 21:18:32 +0700

    Valid Anagram

--- Python/S0242.py
+++ Python/S0242.py
@@ -1,7 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
-            return False     
+            return False
         counter  = {}
         for char in s:
             counter[char] = counter.get(char, 0) + 1
